e6b57fa97718810490e5be24a32f494013f1b20f
monusbence <72020098+monusbence@users.noreply.github.com>
2024-11-14 14:19:03 +0100

    Add files via upload

---
+++ memoria/memoria/memoria/Card.cs
@@ -0,0 +1,13 @@
+﻿namespace memoria
+{
+    internal class Card
+    {
+        public int Number { get; set; }          // A kártyán lévő szám
+        public bool IsMatched { get; set; } = false; // Jelzi, hogy a kártya már párosítva lett-e
+
+        public Card(int number)
+        {
+            Number = number;
+        }
+    }
+}
